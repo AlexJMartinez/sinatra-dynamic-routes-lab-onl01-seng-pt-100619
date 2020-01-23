1fdb7571f9f093cec01204103043318268b25535
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-23 05:19:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,8 +26,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2'
-  number1 = params[:number1]
+  number1 = params[:number1].to_i
   number2 = params[:number2]
+
+  case "add"
+  when ()
 end
 
 
